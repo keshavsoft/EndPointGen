9f3e82b175f29420f1d338d54c010ca2057a881d
keshavsoft <nknnkumar@yahoo.com>
2026-03-27 07:55:38 +0530

    app.js working in editor

--- extension.js
+++ extension.js
@@ -1,4 +1,4 @@
-import { registerAllCommands } from './src/V8/registerCommands.js';
+import { registerAllCommands } from './src/V9/registerCommands.js';
 
 export function activate(context) {
 	registerAllCommands(context);

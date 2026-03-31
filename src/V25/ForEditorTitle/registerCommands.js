119fbba6f229228b523bdf9714548303f58819c1
keshavsoft <nknnkumar@yahoo.com>
2026-03-31 23:47:55 +0530

    app fix perfect

--- src/V25/ForEditorTitle/registerCommands.js
+++ src/V25/ForEditorTitle/registerCommands.js
@@ -4,7 +4,7 @@ import { createEndpointCommand } from '../CommonCommands/CreateEndpoint/start.js
 import { addSubRouteCommand } from '../CommonCommands/AddSubRoute/start.js';
 import { addEndPointCommand } from '../CommonCommands/AddEndPoint/start.js';
 // import { appJsFileCommand } from '../CommonCommands/FixCommands/V1/AppJsFile/start.js';
-import { appJsFileCommand } from '../CommonCommands/FixCommands/V2/AppJsFile/start.js';
+import { appJsFileCommand } from '../CommonCommands/FixCommands/V3/AppJsFile/start.js';
 
 export function registerAllCommands(context) {
     const CreateEndpoint = vscode.commands.registerCommand('extension.editor.title.createEndpoint', createEndpointCommand(context));

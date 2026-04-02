336fa845ef05169c5350bc5f3951a4f7fdf8766d
keshavsoft <nknnkumar@yahoo.com>
2026-04-02 15:17:10 +0530

    still working on routes js file

--- extension.js
+++ extension.js
@@ -1,4 +1,4 @@
-import { registerAllCommands } from './src/V26/registerCommands.js';
+import { registerAllCommands } from './src/V27/registerCommands.js';
 
 export function activate(context) {
 	registerAllCommands(context);

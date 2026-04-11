0d72014ff66e2d82b1b5baf3dfa89b125a086900
keshavsoft <nknnkumar@yahoo.com>
2026-04-11 23:19:41 +0530

    from config just now started

--- src/V16/ForEditorTitle/registerCommands.js
+++
@@ -1,9 +0,0 @@
-import * as vscode from 'vscode';
-
-import { createEndpointCommand } from '../CommonCommands/CreateEndpoint/start.js';
-
-export function registerAllCommands(context) {
-    const CreateEndpoint = vscode.commands.registerCommand('extension.editor.title.createEndpoint', createEndpointCommand(context));
-
-    context.subscriptions.push(CreateEndpoint);
-};
--- src/V29/ForEditorTitle/endpointCommands.js
+++ src/V29/ForEditorTitle/endpointCommands.js
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 
-import { getEndPointCommand } from '../CommonCommands/ForEndPointsJsFile/GetEndPoint/V5/start.js';
+import { getEndPointCommand } from '../CommonCommands/ForEndPointsJsFile/GetEndPoint/V6/start.js';
 import { postEndPointCommand } from '../CommonCommands/ForEndPointsJsFile/PostEndPoint/V6/start.js';
 import { DeleteEndPointCommand } from '../CommonCommands/ForEndPointsJsFile/DeleteEndPoint/V6/start.js';
 import { AlterEndPointCommand } from '../CommonCommands/ForEndPointsJsFile/AlterEndPoint/V6/start.js';

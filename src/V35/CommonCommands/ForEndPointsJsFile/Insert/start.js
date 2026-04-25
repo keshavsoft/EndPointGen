71ac4b6553175f9bf8c9c65945be2599927bad67
keshavsoft <nknnkumar@yahoo.com>
2026-04-25 09:54:00 +0530

    1.35.3 show, find, insert perfect

--- src/V35/CommonCommands/ForEndPointsJsFile/Insert/start.js
+++ src/V35/CommonCommands/ForEndPointsJsFile/Insert/start.js
@@ -1,6 +1,6 @@
 import * as vscode from 'vscode';
 
-import { startOrchestration } from './V3/orchestration/startOrchestration.js';
+import { startOrchestration } from './V4/orchestration/startOrchestration.js';
 
 export async function insertCommand(context, uri) {
     uri = uri || vscode.window.activeTextEditor?.document?.uri;
